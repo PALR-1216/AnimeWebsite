e8b22f66574b1544ffcf151e88a7ed0358eade30
PALR-1216 <palr12162000@gmail.com>
2022-04-01 01:10:24 -0400

    build 1

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const body_parse = require('body-parser')
+const path = require('path')
+const mysql = require('mysql')
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs')
+
+var urlencodedParser = body_parse.urlencoded({extended:false});
+
+
+
+var conn = mysql.createConnection({
+    host:'bn8cepiflg4apyvypdto-mysql.services.clever-cloud.com',
+    user:'uuqcbzq048lebkxt',
+    password:'s6OHD2KAlVjCZ59pYYNv',
+    port:3306,
+    database:'bn8cepiflg4apyvypdto'
+});
+
+
+conn.connect((err) =>{
+    if(err) throw err.sqlMessage;
+    console.log("connected to database")
+
+  
+})
+
+app.get("/", (req,res) =>{
+
+    conn.query("select * from myAnimeShows", (err, rows) =>{
+        var data;
+        res.render('Home', {model:rows})
+      
+        
+       
+    })
+    
+})
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT ,() =>{
+    console.log("server running in port 3000")
+})
